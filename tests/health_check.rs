853beea3d44836b06386ca09699be681ad0fbc2f
Kevin Gómez <kevingomezh@gmail.com>
2023-02-09 13:42:00 +0000

    Configuration management

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,8 @@
 use std::net::TcpListener;
 
+use sqlx::{PgConnection, Connection};
+use zero2prod::configuration::get_configuration;
+
 fn spawn_app() -> String {
   let listener = TcpListener::bind("127.0.0.1:0")
     .expect("Failed to bind random port");
@@ -33,6 +36,13 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
   // Arrange
   let app_address = spawn_app();
+  let config = get_configuration()
+    .expect("Failed to read configuration");
+  let connection_string = config.database.connection_string();
+  let mut connection = PgConnection::connect(&connection_string)
+    .await
+    .expect("Failed to connect to Postgres.");
+
   let client = reqwest::Client::new();
 
   // Act
@@ -47,6 +57,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
   // Assert
   assert_eq!(200, response.status().as_u16());
+
+  let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+    .fetch_one(&mut connection)
+    .await
+    .expect("Failed to fetch saved subscription");
+  
+  assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+  assert_eq!(saved.name, "le guin");
 }
 
 
